3b237f8ea6b0e9f6bd136678a0b9a03f40e54e5f
junho <willight1@naver.com>
2024-11-25 20:46:54 +0900

    USER FBV버젼 로그인 -sign in 로그아웃 - sign out 회원가입 - sign up 회원탈퇴 - delete user 비밀번호 수정 -updatepassword ( 같은비밀번호로는 수정불가) 프로필 조회 - profile 프로필 수정 - updata profile

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-	
+	path('sign_up/', views.sign_up),
+	path('sign_in/', views.sign_in),
+	path('sign_out/', views.sign_out),
+	path('delete/',views.delete_user),
+	path('updatepassword/', views.updatepassword),
+	path('profile/<int:pk>/', views.profile, name='profile')
 ]
